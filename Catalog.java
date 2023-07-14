2b5c94d18ed0524525b7bcdd63e01a6074c222b3
Vitalik <vitalijzaxarov99@mail.ru>
2023-07-14 12:15:21 +0300

    Задание по java финал

---
+++ Catalog.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Catalog extends Laptop {
+
+    public Catalog(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+ 
+}
